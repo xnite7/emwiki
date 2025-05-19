c94b2381739db79cbe213a99bb65d392259fc2b3
xnite7 <xnite7@gmail.com>
2025-05-20 01:16:05 +0400

    rr

--- js/scammers.js
+++ js/scammers.js
@@ -45,17 +45,20 @@ async function createScammerBlock(scammer, container) {
 
 
       block.innerHTML = `
-        <img class="scammerimg" src="${imageUrl}" alt="Avatar of ${robloxUser}" />
-        <div class="gradient"></div>
-        <h2>${robloxUser}</h2>
-        <p><strong>Discord User:</strong> ${discordDisplay}</p>
-        <a href="${robloxProfile}" class="tour-button" style="text-decoration-line: blink;">
-          View Roblox Profile
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M12 5l7 7-7 7"></path>
-            <path d="M5 12h14"></path>
-          </svg>
-        </a>
+        <div class="scammer-content">
+          <img class="scammer-img" src="${imageUrl}" alt="Avatar of ${robloxUser}" />
+          <div class="scammer-info">
+            <h2>${robloxUser}</h2>
+            <p><strong>Discord User:</strong> ${discordDisplay}</p>
+            <a href="${robloxProfile}" class="tour-button">
+              View Roblox Profile
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M12 5l7 7-7 7"></path>
+                <path d="M5 12h14"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
       `;
 
     } catch (error) {
