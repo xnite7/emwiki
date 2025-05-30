3a2f56d058e86487bc97849d5fceb87b6cb94d03
xnite7 <xnite7@gmail.com>
2025-05-31 01:52:12 +0400

    dwa

--- js/scammers.js
+++ js/scammers.js
@@ -68,3 +68,10 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Failed to fetch scammers:", err);
     });
 });
+//<a href="${robloxProfile}" class="tour-button">
+ //         View Roblox Profile
+  //        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+   //         <path d="M12 5l7 7-7 7"></path>
+   //         <path d="M5 12h14"></path>
+    //      </svg>
+     //   </a>
