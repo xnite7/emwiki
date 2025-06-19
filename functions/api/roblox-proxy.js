31569b2bab94f395549c3f3a9576c904b2c38b3f
xnite7 <xnite7@gmail.com>
2025-06-19 06:22:32 +0400

    ubh

--- functions/api/roblox-proxy.js
+++ functions/api/roblox-proxy.js
@@ -1,5 +1,5 @@
 const CACHE_KEY = "discord-scammers";
-const CACHE_TTL_MS = 1000;
+const CACHE_TTL_MS = 24 * 60 * 60 * 1000;
 
 export async function onRequestGet({ request, env }) {
   const url = new URL(request.url);
