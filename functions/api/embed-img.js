92eaab8fbe4f2d800ab2c059a88b475aa42f5cdd
xnite7 <xnite7@gmail.com>
2025-08-02 21:10:56 +0400

    cw

--- functions/api/embed-img.js
+++ functions/api/embed-img.js
@@ -51,37 +51,56 @@ export async function onRequest(context) {
 
         // Build SVG string
         const svg = `
-      <svg width="600" height="800" xmlns="http://www.w3.org/2000/svg">
-    <style>
-  @font-face {
-      font-family: 'Bunny Flowers';
-      font-style: normal;
-      font-weight: normal;
-      src: url("https://emwiki.site/fonts/BunnyFlowers-Regular.woff") format('woff');
-  }
-    .sign {
-     -webkit-text-fill-color: rgba(255, 255, 255, 0);
-      color: rgb(255, 255, 255);
-      left: 0px;
-      opacity: 1;
-      text-shadow: none;
-      top: 0px;
-      background: -webkit-linear-gradient(bottom, rgb(255, 255, 0), rgb(36, 255, 90)) text;
-      font: 200 160px "Bunny Flowers";
-    }
+      <svg xmlns="http://www.w3.org/2000/svg" width="600" height="620">
+      <style>
+        @font-face {
+            font-family: 'Bunny Flowers';
+            font-style: normal;
+            font-weight: normal;
+            src: url("https://emwiki.site/fonts/BunnyFlowers-Regular.woff") format('woff');
+        }
+
+        @font-face {
+            font-family: 'Source Sans Pro';
+            font-style: normal;
+            font-weight: 400;
+            src: local('Source Sans Pro'), url('https://fonts.cdnfonts.com/s/12183/SourceSansPro-Regular.woff') format('woff');
+        }
+
+        text{
+            font-family: 'Source Sans Pro';
+            text-shadow: -2px -2px 0 #000, 0 -2px 0 #000, 2px -2px 0 #000, 2px 0 0 #000, 2px 2px 0 #000, 0 2px 0 #000, -2px 2px 0 #000, -2px 0 0 #000;
+            }
+            .sign {
+            -webkit-text-fill-color: rgba(255, 255, 255, 0);
+            color: rgb(255, 255, 255);
+            left: 0px;
+            opacity: 1;
+            text-shadow: none;
+            top: 0px;
+            background: -webkit-linear-gradient(bottom, rgb(255, 255, 0), rgb(36, 255, 90)) text;
+            font: 200 160px "Bunny Flowers";
+            transform: rotate(7deg);
+            text-shadow: -7px -7px 0 #000, 0 -7px 0 #000, 7px -7px 0 #000, 7px 0 0 #000, 7px 7px 0 #000, 0 7px 0 #000, -7px 7px 0 #000, -7px 0 0 #000;
+            
+            }
 
 
 
-  </style>
-        <rect x="10" y="10" width="580" rx="20" ry="20" height="780" fill="${bgColor}" stroke="white" stroke-width="10" />
-        <text class="sign" x="440" y="150" font-family="Bunny Flowers" font-weight="bold" fill="white" text-anchor="middle" dominant-baseline="middle" filter="url(#shadow)">EC</text>
-        <text x="300" y="655" font-family="Bunny Flowers" font-size="50" font-weight="bold" fill="white" text-anchor="middle" dominant-baseline="middle" filter="url(#shadow)">
+        </style>
+        <rect x="10" y="10" width="580" rx="20" ry="20" height="600" fill="${bgColor}" stroke="white" stroke-width="10"/>
+        <text xmlns="http://www.w3.org/2000/svg" class="sign" x="485" y="70" font-family="Bunny Flowers" font-weight="bold" fill="url(#textGradient)" text-anchor="middle" dominant-baseline="middle" filter="url(#shadow)">EC</text>
+        <text xmlns="http://www.w3.org/2000/svg" x="300" y="535" font-family="Sans" font-size="50" font-weight="bold" fill="white" text-anchor="middle" dominant-baseline="middle">
           ${escapeXml(text)}
         </text>
-        <defs>
+        <defs xmlns="http://www.w3.org/2000/svg">
           <filter id="shadow" x="-20%" y="-20%" width="140%" height="140%">
             <feDropShadow dx="2" dy="2" stdDeviation="4" flood-color="black" flood-opacity="0.6"/>
           </filter>
+      <linearGradient id="textGradient" x1="33%" y1="0%" x2="0%" y2="100%">
+        <stop offset="0%" stop-color="#ffff00"/>
+        <stop offset="100%" stop-color="#24ff5a"/>
+      </linearGradient>
         </defs>
       </svg>`;
 
@@ -100,10 +119,10 @@ export async function onRequest(context) {
 // Simple XML escape helper for safe output in SVG
 function escapeXml(unsafe) {
     return unsafe.replace(/[<>&'"]/g, c => ({
-        '<': '&lt;',
-        '>': '&gt;',
+        '<': '<',
+        '>': '>',
         '&': '&amp;',
         '\'': '&apos;',
-        '"': '&quot;'
+        '"': '''
     })[c]);
 }
