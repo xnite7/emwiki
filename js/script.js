31b73ebb6f9ecb75a8636112e2ee26c5f58b74a6
xnite7 <xnite7@gmail.com>
2025-11-03 01:48:06 +0400

    waa

--- js/script.js
+++ js/script.js
@@ -2316,11 +2316,13 @@ class Auth extends EventTarget {
     async render3DPlayerModel(userId, container) {
         if (this._rendering3DModel) return;
         this._rendering3DModel = true;
+        console.log(userId)
 
         if (!userId) {
             container.style.display = 'none';
             return;
         }
+
         try {
             const response = await fetch(`https://emwiki.com/api/roblox-proxy?mode=avatar-3d&userId=${userId}`);
             if (!response.ok) return;
