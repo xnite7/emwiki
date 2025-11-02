2c77bcea9f974648a812d883c45f3369639cd199
xnite7 <xnite7@gmail.com>
2025-11-02 21:39:37 +0400

    xw

--- js/script.js
+++ js/script.js
@@ -2365,6 +2365,10 @@ class Auth extends EventTarget {
                 if (url.includes('rbxcdn.com/')) {
                     hash = url.split('rbxcdn.com/')[1];
                 }
+                 
+
+                // Strip relative path prefixes (./ and ../)
+                hash = hash.replace(/^\.\.?\//g, '');
 
                 console.log('Hash to proxy:', hash);
                 const proxiedUrl = this.getCdnUrl(hash);
