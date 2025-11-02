3efa6f8707f767c5d840e56306a8a6684aa9a40b
xnite7 <xnite7@gmail.com>
2025-11-03 00:56:33 +0400

    w

--- js/script.js
+++ js/script.js
@@ -356,7 +356,6 @@ class BaseApp {
                     </div>
                     <div id="auth-step-3" style="display: none;">
                         <div id="player-model-container"></div>
-                        <div class="celebration-icon">😻</div>
                         <div class="celebration-title">Account Linked!</div>
                         <p>Welcome to Epic Catalogue! Your Roblox account has been successfully linked.
                         </p>
