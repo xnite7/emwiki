35bbc040301d1dffcb3f003bb423978b7ba95c72
xnite7 <xnite7@gmail.com>
2026-01-24 00:22:53 +0400

    w

--- .wrangler/tmp/dev-BDa8Fd/functionsWorker-0.17996048090183137.js
+++ .wrangler/tmp/dev-BDa8Fd/functionsWorker-0.17996048090183137.js
@@ -8512,7 +8512,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-SLbANh/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-dTa3PC/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -8544,7 +8544,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-SLbANh/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-dTa3PC/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class ___Facade_ScheduledController__2 {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
