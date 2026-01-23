cfd0e4f810e57434a662746775e49fd5d3c77937
xnite7 <xnite7@gmail.com>
2026-01-24 00:41:00 +0400

    w

--- .wrangler/tmp/pages-0ICVPy/functionsWorker-0.17996048090183137.mjs
+++ .wrangler/tmp/pages-0ICVPy/functionsWorker-0.17996048090183137.mjs
@@ -7761,10 +7761,10 @@ var init_functionsRoutes_0_10531370690170583 = __esm({
   }
 });
 
-// ../.wrangler/tmp/bundle-oGQ3bM/middleware-loader.entry.ts
+// ../.wrangler/tmp/bundle-YlVDxC/middleware-loader.entry.ts
 init_functionsRoutes_0_10531370690170583();
 
-// ../.wrangler/tmp/bundle-oGQ3bM/middleware-insertion-facade.js
+// ../.wrangler/tmp/bundle-YlVDxC/middleware-insertion-facade.js
 init_functionsRoutes_0_10531370690170583();
 
 // ../../../../AppData/Roaming/npm/node_modules/wrangler/templates/pages-template-worker.ts
@@ -8260,7 +8260,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// ../.wrangler/tmp/bundle-oGQ3bM/middleware-insertion-facade.js
+// ../.wrangler/tmp/bundle-YlVDxC/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -8293,7 +8293,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// ../.wrangler/tmp/bundle-oGQ3bM/middleware-loader.entry.ts
+// ../.wrangler/tmp/bundle-YlVDxC/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
